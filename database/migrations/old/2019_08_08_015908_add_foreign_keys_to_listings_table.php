4fcc1aee6e636c21f4fb58098fb8f64abedb736e
Ivan Aldwin A. Cristobal <ivanaldwin996@gmail.com>
2019-08-30 10:06:02 +0800

    updated migrations

---
+++ database/migrations/old/2019_08_08_015908_add_foreign_keys_to_listings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddForeignKeysToListingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('listings', function(Blueprint $table)
+		{
+			$table->foreign('driver_id', 'listings_ibfk_1')->references('id')->on('users')->onUpdate('CASCADE')->onDelete('CASCADE');
+			$table->foreign('car_id', 'listings_ibfk_2')->references('car_id')->on('cars')->onUpdate('CASCADE')->onDelete('CASCADE');
+			$table->foreign('city_id', 'listings_ibfk_3')->references('city_id')->on('cities')->onUpdate('CASCADE')->onDelete('CASCADE');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('listings', function(Blueprint $table)
+		{
+			$table->dropForeign('listings_ibfk_1');
+			$table->dropForeign('listings_ibfk_2');
+			$table->dropForeign('listings_ibfk_3');
+		});
+	}
+
+}
