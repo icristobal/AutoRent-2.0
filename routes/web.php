cd30bc3b108f02e098e3890f45b6b1c4bb9af064
Ivan Aldwin A. Cristobal <ivanaldwin996@gmail.com>
2019-09-03 07:48:43 +0800

    added profile photo functions

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,12 @@ Route::get('/', function () { return view('welcome');});
 Route::get('/about-us', 'UserController@aboutus')->name('about-us');
 Route::get('/contact-us', 'UserController@contactus')->name('home');
 Route::get('/findcar', 'UserController@findcar')->name('findcar');
+
 Auth::routes();
-//Logged-in users group: acts as(/admin/home or /driver/home)
+
 Route::group(['middleware' => 'auth'], function () {
 	
-	//ADMIN
+	#ADMIN
 	Route::group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function () {
 		Route::prefix('admin')->group(function() { 
 			Route::get('/home', 'AdminController@index')->name('admin-home');
@@ -27,31 +28,35 @@ Route::group(['middleware' => 'auth'], function () {
 		});
 	});
 
-	//DRIVER
+	#DRIVER
 	Route::group(['middleware' => 'App\Http\Middleware\DriverMiddleware'], function () {
 		Route::prefix('driver')->group(function() {
 			Route::get('/home', 'DriverController@index')->name('driver-home');
 			Route::resource('/insertlist','InsertListingsController');
 			Route::resource('/insertcar','InsertCarController');
 			Route::resource('/my-jobs','DriverJobsController');
-			//Status
+
+			#Job Status
 			Route::post('/my-jobs/approve/{id}', 'DriverJobsController@postApprove')->name('approvetxn');
 			Route::post('/my-jobs/done/{id}', 'DriverJobsController@postDone')->name('donetxn');
 			Route::post('/my-jobs/reject/{id}', 'DriverJobsController@postReject')->name('denytxn');
 			Route::post('/my-jobs/cancel/{id}', 'DriverJobsController@postCancel')->name('canceltxn');
 
+			#My Profile Routes
 			Route::resource('/my-profile','DriverProfileController');
 			Route::get('/my-transactions','TxnHistoryController@driver')->name('driver-txn');
 			Route::get('/changepass','DriverProfileController@showChangePassword')->name('changepass');
 			Route::post('/changePassword','DriverProfileController@changePassword')->name('changePassword');
-	
+			Route::post('/profilepic','ProfilePicController@updatepic')->name('updatepic');
+			Route::get('/profilepic','ProfilePicController@driverShow')->name('dpage-show');
+
 			Route::get('/my-profile/update/', [
         	'as' => 'my-profile/update',
       	  	'uses' => 'DriverProfileController@update']);
 		});
 	});
 	
-	//USER
+	#USER
 	Route::group(['middleware' => 'App\Http\Middleware\UserMiddleware'], function () {
 		Route::prefix('user')->group(function() {
 			Route::get('/home', 'UserController@index')->name('user-home');
@@ -59,18 +64,20 @@ Route::group(['middleware' => 'auth'], function () {
 			Route::get('/contact-us', 'UserController@contactus')->name('contact-us');
 			Route::resource('/findcar', 'ListingController');
 			Route::get('/findcar/{id}','ListingController@show')->name('cardetails');
-			Route::resource('/myprofile','UserProfileController');
 			Route::get('/my-transactions','TxnHistoryController@user')->name('user-txn');
 			Route::resource('/search','UserSearchController');
 			Route::get('/s-result','UserSearchController@search')->name('s-result');
+
 			#My Profile Routes
 			Route::resource('/my-profile','UserProfileController');
 			Route::get('/changepass','UserProfileController@showChangePassword')->name('changepass');
 			Route::post('/changePassword','UserProfileController@changePassword')->name('changePassword');
+			Route::post('/profilepic','ProfilePicController@updatepic')->name('updatepic');
+			Route::get('/profilepic','ProfilePicController@userShow')->name('upage-show');
+
 			Route::get('/my-profile/update/{id}', [
        		'as' => 'my-profile/update',
        		'uses' => 'UserProfileController@update']);
-       		
 		});
 	});
 });
