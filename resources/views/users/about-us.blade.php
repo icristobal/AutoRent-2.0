f0cbf90af94ccb13f55cdd6ee031c99523457c31
Ivan Aldwin A. Cristobal <ivanaldwin996@gmail.com>
2019-09-03 16:05:37 +0800

    added card div

--- resources/views/users/about-us.blade.php
+++ resources/views/users/about-us.blade.php
@@ -6,17 +6,19 @@
 @section('content')
 
 <div class="container">
-	<h3>About Us</h3>
-	<hr>
-	<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis iure voluptatum, 
-	repellendus reiciendis suscipit, debitis laboriosam nesciunt doloremque sequi. Iure 
-	enim magni eveniet itaque perferendis fugiat sapiente consectetur quaerat quia!</p>
+    <h3>About Us</h3>
+    <hr>
+    <div class="card card-body">
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis iure voluptatum,
+            repellendus reiciendis suscipit, debitis laboriosam nesciunt doloremque sequi. Iure
+            enim magni eveniet itaque perferendis fugiat sapiente consectetur quaerat quia!</p>
+    </div>
 </div>
 
 @endsection
