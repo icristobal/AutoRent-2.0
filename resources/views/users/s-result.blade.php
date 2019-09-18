09c0beb842cced7176a93ef694c6d417e013c58b
Ivan Aldwin A. Cristobal <ivanaldwin996@gmail.com>
2019-09-18 12:09:30 +0800

    added images and pagename

--- resources/views/users/s-result.blade.php
+++ resources/views/users/s-result.blade.php
@@ -1,24 +1,26 @@
 @extends('layouts.l-gen')
 @section('content')
+@section('pagename', 'Search Results')
 
 <div class="container">
     <h1 class="h1 float-left">Search Results</h1>
     <br><br>
     <hr>
     <div class="card-group">
-        @foreach($data as $posted)
-        <div class="card">
-            <div class="card-body">
-                <img src="..." class="card-img-top" alt="...">
-                <input type="hidden" id="listing_id" name="listing_id" value="{{$posted->listing_id}}">
-                <h2 class="card-title">{{$posted->listing_title}} by {{$posted->name}} </h5>
-                <h4 class="card-text">PHP {{$posted->rate}}</h3>
-                <p class="card-text">{{$posted->notes}}</p>
-                <a href="{{ route('cardetails', $posted->listing_id) }}">View</a>
-                </a>
+            @foreach($data as $posted)
+            <div class="card">
+                <div class="card-body">
+                    <img src="data:image/png;base64,{{ chunk_split(base64_encode($posted->listing_image)) }}" width="300" height="245">
+                    <hr>
+                    <input type="hidden" id="listing_id" name="listing_id" value="{{$posted->listing_id}}">
+                    <h2 class="card-title">{{$posted->listing_title}}</h5>
+                    <p class="card-text">{{$posted->name}}</p>
+                    <h4 class="card-text">PHP {{$posted->rate}}</h3>
+                    <p class="card-text">{{$posted->notes}}</p>
+                    <a href="{{ route('cardetails', $posted->listing_id) }}" class="btn btn-primary">View</a>
+                </div>
             </div>
-        </div>
-        @endforeach
+            @endforeach
     </div>
 </div>
   <div class="mt-lg-5"></div>
